aef06664c851646e514f87a39b5640a2930d9d12
dalisyron <mobindariush@outlook.com>
2019-04-24 15:40:03 +0430

    Add solution for riddles 9 to 11

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle10.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle10.kt
@@ -1,6 +1,7 @@
 package com.vanniktech.rxriddles
 
 import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
 
 object Riddle10 {
   /**
@@ -9,6 +10,9 @@ object Riddle10 {
    * Use case: Get some user data and perform a network request with the user data and have both data accessible afterwards.
    */
   fun solve(first: Observable<Int>, function: (Int) -> Observable<String>): Observable<Pair<Int, String>> {
-    TODO()
+    return first.flatMap {
+      it -> Observable.combineLatest<Int,String,Pair<Int,String>>(Observable.just(it), function(it), BiFunction{ x, y -> x to y})
+    }
+
   }
 }
--- src/main/kotlin/com/vanniktech/rxriddles/Riddle9.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle9.kt
@@ -9,6 +9,15 @@ object Riddle9 {
    * Use case: Cancel an Observable when something has happened. For instance, stop polling when the user has been logged out.
    */
   fun solve(main: Observable<Unit>, trigger: Observable<Unit>): Observable<Unit> {
-    TODO()
+    /*
+    var flag = true
+    trigger.doOnNext {
+      flag = false
+    }
+    return main.takeWhile {
+      flag
+    }
+    */
+    return main.takeUntil(trigger)
   }
 }
